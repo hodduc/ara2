cd17880cfd7958cdcd4c3a5ea156f392e91ef1cb
Brian Hong <serialx@serialx.net>
2013-02-19 23:09:02 +0900

    Move board related models to ara2.board.models.

--- ara2/models.py
+++ ara2/models.py
@@ -2,99 +2,12 @@
 from django.db import models
 
 
-class AraraSession(models.Model):
-    session_key = models.CharField(max_length=120, primary_key=True)
-    session_data = models.TextField(blank=True)
-    expire_date = models.DateTimeField(null=True, blank=True)
-
-
-class ArticleVoteStatus(models.Model):
-    board_id = models.ForeignKey('Board')
-    article_id = models.ForeignKey('Article')
-    user_id = models.ForeignKey('User')
-
-
-class Article(models.Model):
-    title = models.CharField(max_length=600, blank=True)
-    board_id = models.ForeignKey('Board', null=True, blank=True)
-    heading_id = models.ForeignKey('BoardHeading', null=True, blank=True)
-    content = models.TextField(blank=True)
-    author_id = models.ForeignKey('User', null=True, blank=True)
-    author_nickname = models.CharField(max_length=120, blank=True)
-    author_ip = models.CharField(max_length=45, blank=True)
-    date = models.DateTimeField(null=True, blank=True)
-    hit = models.IntegerField(null=True, blank=True)
-    positive_vote = models.IntegerField(null=True, blank=True)
-    negative_vote = models.IntegerField(null=True, blank=True)
-    deleted = models.IntegerField(null=True, blank=True)
-    root_id = models.ForeignKey('Article', related_name='articles', null=True, blank=True)
-    parent_id = models.ForeignKey('Article', related_name='+', null=True, blank=True)
-    reply_count = models.IntegerField()
-    is_searchable = models.IntegerField()
-    last_modified_date = models.DateTimeField(null=True, blank=True)
-    destroyed = models.IntegerField(null=True, blank=True)
-    last_reply_date = models.DateTimeField(null=True, blank=True)
-    last_reply_id = models.IntegerField(null=True, blank=True)
-
-
 class Banner(models.Model):
     content = models.TextField(blank=True)
     issued_date = models.DateTimeField(null=True, blank=True)
     due_date = models.DateTimeField(null=True, blank=True)
     valid = models.IntegerField(null=True, blank=True)
     weight = models.IntegerField(null=True, blank=True)
-
-
-class BbsManager(models.Model):
-    board_id = models.ForeignKey('Board', null=True, blank=True)
-    manager_id = models.ForeignKey('User', null=True, blank=True)
-
-
-class Blacklist(models.Model):
-    user_id = models.ForeignKey('User', null=True, blank=True)
-    blacklisted_user_id = models.ForeignKey('User', related_name='+', null=True, blank=True)
-    blacklisted_date = models.DateTimeField(null=True, blank=True)
-    last_modified_date = models.DateTimeField(null=True, blank=True)
-    block_article = models.IntegerField(null=True, blank=True)
-    block_message = models.IntegerField(null=True, blank=True)
-
-
-class BoardHeading(models.Model):
-    board_id = models.ForeignKey('Board', null=True, blank=True)
-    heading = models.CharField(max_length=90, blank=True)
-
-
-class BoardNotice(models.Model):
-    article = models.ForeignKey('Article', primary_key=True)
-
-
-class Board(models.Model):
-    category_id = models.ForeignKey('Category', null=True, blank=True)
-    board_name = models.CharField(max_length=90, unique=True, blank=True)
-    board_alias = models.CharField(max_length=90, blank=True)
-    board_description = models.CharField(max_length=900, blank=True)
-    deleted = models.IntegerField(null=True, blank=True)
-    read_only = models.IntegerField(null=True, blank=True)
-    hide = models.IntegerField(null=True, blank=True)
-    order = models.IntegerField(null=True, blank=True)
-    type = models.IntegerField(null=True, blank=True)
-    to_read_level = models.IntegerField(null=True, blank=True)
-    to_write_level = models.IntegerField(null=True, blank=True)
-
-
-class Category(models.Model):
-    category_name = models.CharField(max_length=90, unique=True, blank=True)
-    order = models.IntegerField(null=True, blank=True)
-
-
-class File(models.Model):
-    filename = models.CharField(max_length=600, blank=True)
-    saved_filename = models.CharField(max_length=600, blank=True)
-    filepath = models.TextField(blank=True)
-    user_id = models.ForeignKey('User', null=True, blank=True)
-    board_id = models.ForeignKey('Board', null=True, blank=True)
-    article_id = models.ForeignKey('Article', null=True, blank=True)
-    deleted = models.IntegerField(null=True, blank=True)
 
 
 class Link(models.Model):
@@ -126,30 +39,6 @@ class Message(models.Model):
     received_deleted = models.IntegerField(null=True, blank=True)
     sent_deleted = models.IntegerField(null=True, blank=True)
     read_status = models.CharField(max_length=3, blank=True)
-
-
-class ReadStatus(models.Model):
-    user_id = models.ForeignKey('User', null=True, blank=True)
-    article_id = models.ForeignKey('Article', null=True, blank=True)
-    status = models.CharField(max_length=3, blank=True)
-
-
-class ReadStatusOrg(models.Model):
-    user_id = models.ForeignKey('User', null=True, blank=True)
-    board_id = models.ForeignKey('Board', null=True, blank=True)
-    read_status_data = models.TextField(blank=True)
-    read_status_numbers = models.TextField(blank=True)
-    read_status_markers = models.TextField(blank=True)
-
-
-class ScrapStatus(models.Model):
-    user_id = models.ForeignKey('User')
-    article_id = models.ForeignKey('Article')
-
-
-class SelectedBoard(models.Model):
-    user_id = models.ForeignKey('User', null=True, blank=True)
-    board_id = models.ForeignKey('Board', null=True, blank=True)
 
 
 class UserActivation(models.Model):
--- ara2/settings.py
+++ ara2/settings.py
@@ -120,6 +120,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'ara2',
+    'ara2.board',
     'oldara',
 )
 
