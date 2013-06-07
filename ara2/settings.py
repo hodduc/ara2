8bb1f1da84729d4d317a4e1cc9ce29d9d7aa86f3
이준성 <hodduc@sparcs.org>
2013-06-07 22:35:36 +0900

    Add settings for old ara migration. Add local settings import.

--- ara2/settings.py
+++ ara2/settings.py
@@ -17,6 +17,14 @@ DATABASES = {
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    },
+    'oldara': {
+        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'ara_legacy',                      # Or path to database file if using sqlite3.
+        'USER': 'ara2',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
 
@@ -159,3 +167,8 @@ REST_FRAMEWORK = {
 }
 
 AUTH_USER_MODEL = 'account.User'
+
+try:
+    from local_settings import *
+except ImportError:
+    pass
